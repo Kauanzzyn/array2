9b29645674d43fefb6b094255cf720b9d13cefa4
Kauanzzyn <kauan_a_rocha@estudante.sesisenai.org.br>
2023-03-15 09:39:27 -0300

    ataulização2

--- vetor.js
+++ vetor.js
@@ -1,14 +1,16 @@
 // variavel global do tipo array
 let valor_cidade = [] 
-
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
 }
 
 function excluir(){
     valor_cidade.pop()
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
 }
